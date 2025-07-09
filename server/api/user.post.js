7e2f8479f61c36ca5acf6fad776fe4a6db7bca40
Angelo <angelowilliams1015@gmail.com>
2025-07-09 20:12:40 +0200

    bcrypt and salt added

--- server/api/user.post.js
+++ server/api/user.post.js
@@ -1,15 +1,36 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default defineEventHandler(async (event) => {
+//   const body = await readBody(event);
+//   await prisma.user.create({
+//     data: {
+//       email: body.email,
+//       password: body.password,
+//     },
+//   });
+
+//   return { data: "User registration endpoint" };
+// });
+
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(body.password, salt);
+
+  // sends to database for reference
   await prisma.user.create({
     data: {
-      // Use 'data' instead of 'body'
       email: body.email,
-      password: body.password,
+      password: passwordHash,
+      salt: salt,
     },
   });
 
